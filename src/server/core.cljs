c578fc7ecdcef862b910e4e161148f5695f5bce1
Heinz N. Gies <heinz@licenser.net>
2012-04-26 01:13:58 +0200

    Added check for admin network.

--- src/server/core.cljs
+++ src/server/core.cljs
@@ -38,11 +38,13 @@
 
 (defn start [& _]
   (storage/init)
-  (let [server (.createServer http handler)
-        port (get-in @storage/data [:server :port] 80)
-        host (get-in @storage/data [:server :host] "0.0.0.0")]
+  (if (get-in @storage/data [:network :admin])
+    (let [server (.createServer http handler)
+          port (get-in @storage/data [:server :port] 80)
+          host (get-in @storage/data [:server :host] "0.0.0.0")]
 
-    (.listen server port host)
-    (println "Server running at" (str "http://" host ":" port "/"))))
+      (.listen server port host)
+      (println "Server running at" (str "http://" host ":" port "/")))
+    (println "Error: no admin network specified, please use ./client.sh to create it!")))
 
 (set! *main-cli-fn* start)
